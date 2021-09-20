4ca65995d7e8e16a6363ef57ec59340b6df22648
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2021-09-20 07:41:19 -0400

    refactor: Ajout de fonctions pour des affichages📦

--- canvascii.c
+++ canvascii.c
@@ -1,10 +1,11 @@
-// Inclue les fonstionnalités pour les sorties et les entrées standards
+// Provides many standard library functions for file input and output
 #include <stdio.h>
 
-// Déclarations de constantes
+// Declaration of constants
 #define MAX_HEIGHT 40
 #define MAX_WIDTH 80
-#define USAGE "\
+#define USAGE                                                                  \
+  "\
 Usage: %s [-n HEIGHT,WIDTH] [-s] [-k] [-p CHAR]\n\
           [-h ROW] [-v COL] [-r ROW,COL,HEIGHT,WIDTH]\n\
           [-l ROW1,COL1,ROW2,COL2] [-c ROW,COL,RADIUS]\n\
@@ -50,36 +51,50 @@ struct canvas {
 
 // Declaration of a list of code errors
 enum error {
-  OK                         = 0, // Everything is ok
-  ERR_WRONG_PIXEL            = 1, // Wrong pixel value in canvas
-  ERR_CANVAS_TOO_HIGH        = 2, // Canvas is too high
-  ERR_CANVAS_TOO_WIDE        = 3, // Canvas is too wide
+  OK = 0,                         // Everything is ok
+  ERR_WRONG_PIXEL = 1,            // Wrong pixel value in canvas
+  ERR_CANVAS_TOO_HIGH = 2,        // Canvas is too high
+  ERR_CANVAS_TOO_WIDE = 3,        // Canvas is too wide
   ERR_CANVAS_NON_RECTANGULAR = 4, // Canvas is non rectangular
-  ERR_UNRECOGNIZED_OPTION    = 5, // Unrecognized option
-  ERR_MISSING_VALUE          = 6, // Option with missing value
-  ERR_WITH_VALUE             = 7  // Problem with value
+  ERR_UNRECOGNIZED_OPTION = 5,    // Unrecognized option
+  ERR_MISSING_VALUE = 6,          // Option with missing value
+  ERR_WITH_VALUE = 7              // Problem with value
 };
 
 // Declaration of a list of pixels
 enum pixels {
-  EMPTY   = '.',  // empty pixel
-  BLACK   = 0,    // black pixel
-  RED     = 1,    // red pixel
-  GREEN   = 2,    // green pixel
-  YELLOW  = 3,    // yellow pixel
-  BLUE    = 4,    // blue pixel
-  MAGENTA = 5,    // magenta pixel
-  CYAN    = 6,    // cyan pixel
-  WHITE   = 7,    // white pixel
+  EMPTY = '.', // empty pixel
+  BLACK = 0,   // black pixel
+  RED = 1,     // red pixel
+  GREEN = 2,   // green pixel
+  YELLOW = 3,  // yellow pixel
+  BLUE = 4,    // blue pixel
+  MAGENTA = 5, // magenta pixel
+  CYAN = 6,    // cyan pixel
+  WHITE = 7,   // white pixel
 };
 
-int main(int argc, char *argv[]) {
-  /**
-   * Print the number of arguments and the arguments names to the console
-   **/
+/**
+ * Print the number of arguments and the arguments names to the console
+ **/
+void printArguments(int argc, char *argv[]) {
   printf("argc = %d\n", argc);
   for (unsigned int i = 0; i < argc; ++i) {
     printf("argv[%d] = %s\n", i, argv[i]);
   }
+}
+
+// Print the usage documentation to the console
+void printUsage() { printf("%s", USAGE); }
+
+//------------------------------------------------------------------------------
+int main(int argc, char *argv[]) {
+  printArguments(argc, argv);
+
+  // If there's no explicit arguments (ex: -n, -s, etc...)
+  if (argc == 1) {
+    printUsage();
+  }
+
   return 0;
 }
